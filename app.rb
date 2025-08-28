72480828af198afa0652958f892142d86765d8fc
Tony Kwong <tonykkn4321@gmail.com>
2025-08-28 10:53:34 +0800

    Ready to deploy to Railway

--- app.rb
+++ app.rb
@@ -2,9 +2,36 @@ require 'sinatra'
 require 'sinatra/activerecord'
 require 'dotenv/load'
 
+# Load database config based on RACK_ENV
+set :environment, ENV['RACK_ENV'] || :development
 set :database_file, 'config/database.yml'
 
+# Require models
 require_relative './models/author'
 require_relative './models/book'
+
+# Require routes
 require_relative './routes/authors'
 require_relative './routes/books'
+
+# Auto-create tables if they don't exist
+configure do
+  ActiveRecord::Base.logger = Logger.new(STDOUT) if settings.development?
+
+  unless ActiveRecord::Base.connection.table_exists?('authors') &&
+         ActiveRecord::Base.connection.table_exists?('books')
+    ActiveRecord::Schema.define do
+      create_table :authors do |t|
+        t.string :first_name
+        t.string :last_name
+      end
+
+      create_table :books do |t|
+        t.string  :title
+        t.integer :year
+        t.integer :author_id
+      end
+    end
+    puts "✅ Tables created"
+  end
+end
